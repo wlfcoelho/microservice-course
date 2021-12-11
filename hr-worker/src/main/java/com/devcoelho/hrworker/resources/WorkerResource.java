cd3e758d66143ac5c29c7ab039fb5626a5cb382e
Wilson Coelho <wlfcoelho@gmail.com>
2021-12-11 08:35:17 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/devcoelho/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devcoelho/hrworker/resources/WorkerResource.java
@@ -1,6 +1,6 @@
 package com.devcoelho.hrworker.resources;
 
-import java.util.List; 
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,6 +37,12 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		try {
+			Thread.sleep(3000L);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
