1a9aafbdf850f663e39794f7dccbacec29f22cf9
Wilson Coelho <wlfcoelho@gmail.com>
2021-12-14 06:25:18 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/com/devcoelho/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcoelho.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
